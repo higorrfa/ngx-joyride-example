65d047a767b5fd32f86a50d709837321094f3c2c
Higor Araújo <higor.araujo@virtus.ufcg.edu.br>
2023-06-29 18:03:41 -0300

    feat: add ngx joyride to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import { JoyrideService } from 'ngx-joyride'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ngx-joyride-example';
+
+  constructor(private readonly joyrideService: JoyrideService) {}
+
+  onClick() {
+    this.joyrideService.startTour({
+      steps: [
+        'firstStep',
+        'secondStep',
+        'thirdStep',
+        'fourthStep',
+        'fifthStep',
+      ],
+      customTexts: {
+        next: '>>',
+        prev: '<<',
+        done: 'Ok',
+      },
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JoyrideModule } from 'ngx-joyride';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    JoyrideModule.forRoot(),
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
